4093b372eaab8a2106d4fa2ec4402b1cc15af7e1
Michael Tom-Wing <mtomwing@gmail.com>
2014-09-27 20:01:43 -0700

    Looking into hangups conversation callbacks

--- pickups/server.py
+++ pickups/server.py
@@ -17,14 +17,17 @@ class Server(object):
             loop.create_server(Protocol, host, port))
 
         self._hangups = hangups.Client(cookies)
-        self._hangups.on_connect.add_observer(self._on_connect)
+        self._hangups.on_connect.add_observer(self._on_hangups_connect)
 
     def run(self):
         loop = asyncio.get_event_loop()
         asyncio.async(self._server.wait_closed())
         loop.run_until_complete(self._hangups.connect())
 
-    def _on_connect(self, initial_data):
+    # Hangups Callbacks
+
+    def _on_hangups_connect(self, initial_data):
+        """Called when hangups successfully auths with hangouts."""
         self._user_list = hangups.UserList(
             self._hangups, initial_data.self_entity, initial_data.entities,
             initial_data.conversation_participants
@@ -33,6 +36,12 @@ class Server(object):
             self._hangups, initial_data.conversation_states, self._user_list,
             initial_data.sync_timestamp
         )
+        self._conv_list.on_event.add_observer(self._on_hangups_event)
+
+    def _on_hangups_event(self, conv_event):
+        """Called when a hangups conversation event occurs."""
+        if isinstance(conv_event, hangups.ChatMessageEvent):
+            print('Event:', conv_event)
 
     # Client Callbacks
 
